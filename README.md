7d7f5db8de5fbef56a9c287db4f2bcb9e8095367
Aashni Shah <contact@aashni.me>
2016-03-03 06:11:38 -0500

    functioning whateverweather chrome extension

---
+++ README.md
@@ -0,0 +1,18 @@
+# WhateverWeather
+Bringing your weather reports right to you.
+
+##How to Use the extension
+1. Download this project
+2. Visit chrome://extensions in your Chrome browser
+3. Ensure **Developer Mode** is checked
+4. Click on **Load unpacked extension..**
+5. Navigate to where these files are on your computer and select them
+6. Click on the icon and see what the current weather is!
+
+##Shout out to:
+1. [Free GEO IP](http://freegeoip.net/json/)
+2. [Forecast IO](https://api.forecast.io/forecast/)
+3. [Icon Works @ Flat Icon](http://www.flaticon.com/authors/icon-works)
+
+## How It Works
+The extenion calls on freegeoip in order to obtain where you are. It then uses this information whilst calling the forecast api. The forecast api returns back the current weather, which is then diplayed on the extension page.
---
+++ popup.js
@@ -0,0 +1,86 @@
+var settings = {
+	geoip_api: 'http://freegeoip.net/json/',
+	forecast_base_url: 'https://api.forecast.io/forecast/',
+	forecast_api_key: 'a956d10ffcc9d53ec8df86a24cbdd5d2'
+}
+var city = '';
+var country = '';
+
+document.addEventListener('DOMContentLoaded', function() {
+	var wwCity = document.getElementById('ww-city');
+	wwCity.innerText = 'Currently processing information. Please hold on!';
+	// get location
+	GET_IP(settings.geoip_api, getForecast);
+});
+
+/* this function performs the get IP call
+*/
+function GET_IP(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	xhr.send();
+	xhr.onload = function() {
+	    var json = xhr.responseText;                         // Response
+	    json = json.replace(/^[^(]*\(([\S\s]+)\);?$/, '$1'); // Turn JSONP in JSON
+	    json = JSON.parse(json);
+		callback(json);
+	};
+}
+
+/* this function performs the get forecast call
+*/
+function GET_FORECAST(url, callback) {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	xhr.send();
+	xhr.onload = function() {
+	    var json = xhr.responseText;                         // Response
+	    json = json.replace(/^[^(]*\(([\S\s]+)\);?$/, '$1'); // Turn JSONP in JSON
+	    json = JSON.parse(json);
+		callback(json);
+	};
+}
+
+/* get forecast for specific location
+*/
+function getForecast(data) {
+	var time = new Date().getTime();
+	time = Math.floor(time / 1000);
+	var url = settings.forecast_base_url + settings.forecast_api_key + '/' +
+				data.latitude + ',' + data.longitude + ',' + time;
+	city = data.city;
+	country = data.country_name;
+	GET_FORECAST(url, display);
+}
+
+/* process and display information on the
+ * html page
+ */
+function display(data) {
+	console.log('display', data);
+
+	if(!data || !city || !country) {
+		var wwCity = document.getElementById('ww-city');
+		wwCity.innerText = 'ERROR processing information. Please try again later.';
+		return;
+	}
+
+	var wwCity = document.getElementById('ww-city');
+	wwCity.innerText = 'You are currently in ' + city + ', ' +  country;
+
+	var wwTemp = document.getElementById('ww-temp');
+	wwTemp.innerText = 'It is currently ' + data.currently.temperature + ' deg F and feels like ' +
+						data.currently.apparentTemperature + ' deg F.';
+
+	var wwSummary = document.getElementById('ww-summary');
+	wwSummary.innerText = data.currently.summary;
+}
+
+/* this eventListener allows us to create a new tab when we click on 
+ * a link in a chrome extension 
+ */
+window.addEventListener('click',function(e){
+  if(e.target.href!==undefined){
+    chrome.tabs.create({url:e.target.href})
+  }
+})
